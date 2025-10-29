7209eebb4d0fa820b226e9ee7e7a971c94f4b4bf
Minh Cat DO <m.do@myskolae.fr>
2025-10-29 01:02:23 +0100

    add helper function get_fk_col_list_index

--- db/helper.c
+++ db/helper.c
@@ -4,8 +4,43 @@ Description : Helper functions for db core functions
 Group 2 ESGI 2A3
 */
 
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
 #include "../main.h"
 
-char** get_fk_col_list(Query){
+int* get_fk_col_list_index(Query query){
+    /*
+    The function returns an array/pointer of index matches with col_refer_list and table_refer_list
+    Example : 
+    col_list = col1 pk, col2, col3 fk, col4, col5 fk
+    fk_index = 2, 4 -> which are indexes of col3 and col5 in col_list
+
+    Returns NULL pointer if there is error
+    */
+    int* res = (int*)malloc(sizeof(int) * query.params.create_params.fk_count);
+    assert(res != NULL);
+    int current_index = 0;
+    int i;
+
+    switch (query.cmd_type)
+    {
+    case CREATE:
+        int col_count = query.params.create_params.col_count;
+        ColConstraintType* constraint_list = query.params.create_params.constraint_list;
+        for(i=0; i<col_count; i++){
+            if(constraint_list[i] == FK){
+                res[current_index] = i;
+                current_index++;
+            }
+        }
+        break;
     
+    default:
+        free(res); // will return NULL if error
+        break;
+    }
+    
+    return res;
 }
