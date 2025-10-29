5ce8942ab2496e96830dcf7e60515d9a97735b32
Minh Cat DO <m.do@myskolae.fr>
2025-10-29 01:09:33 +0100

    create helper.h

---
+++ db/helper.h
@@ -0,0 +1,9 @@
+/*
+Date of creation : 29/10/2025
+Description : Helper functions protypes are declared here
+Group 2 ESGI 2A3
+*/
+
+#include "../main.h"
+
+int* get_fk_col_list_index(Query query);
