6af58f6a0d5ad30b56036d0ab539dbd23f1d575d
Minh Cat DO <m.do@myskolae.fr>
2025-10-31 19:17:07 +0100

    add free single table

--- clean/db.c
+++ clean/db.c
@@ -11,9 +11,9 @@ Group 2 ESGI 2A3
 
 void free_col(Col* col){
     free(col->name);
-    free(col->next_col);
     col->name = NULL;
-    col->next_col = NULL;
+    free(col);
+    col = NULL;
 }
     void free_row(Row* row, int col_count){
     int i;
@@ -25,7 +25,39 @@ void free_col(Col* col){
     free(row->data_field);
     row->data_field = NULL;
 
-    free(row->next_row);
-    row->next_row = NULL;
+    free(row);
+    row = NULL;
 }
-void free_table(); //free col and row from last to first
+void free_table(Table* table){
+    // this is to clean only 1 table, for all table need to free from last table to first
+    int i;
+    Col* current_col = table->first_col;
+    Col* tmp_col; // temporary to free current col
+    Row* current_row = table->first_row;
+    Row* tmp_row;
+
+    free(table->name);
+    table->name = NULL;
+
+    //free col 
+    while(current_col != NULL){
+        tmp_col = current_col;
+        // save pointer to next col, then free current pointer
+        current_col = current_col->next_col;
+        
+        free_col(tmp_col);   
+    }
+    //free row 
+    while(current_row != NULL){
+        tmp_row = current_row;
+        // save pointer to next row, then free current pointer
+        current_row = current_row->next_row;
+        
+        free_row(tmp_row, table->col_count);   
+    }
+
+    free(table);
+    table = NULL;
+} 
+
+// WARNING : must not free pointer to the next one
