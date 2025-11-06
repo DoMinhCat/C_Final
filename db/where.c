d866324af473b39fcb55e4b0e15f5d90ebc31967
Minh Cat DO <m.do@myskolae.fr>
2025-11-06 04:07:07 +0100

    start where clause

---
+++ db/where.c
@@ -0,0 +1,9 @@
+/*
+Date of creation : 06/11/2025
+Description : Code of WHERE clause for SELECT and DELETE
+Group 2 ESGI 2A3
+*/
+
+#include "db.h"
+
+// idea: return a list of pointers to rows that satisfy condition?
