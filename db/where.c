ae4503c963f8404c3e9354a486691a3aec4344f6
Minh Cat DO <m.do@myskolae.fr>
2025-11-16 03:36:02 +0100

    add compare with NULL in WHERE clause

--- db/where.c
+++ db/where.c
@@ -41,17 +41,21 @@ FilteredRow* traverse_and_compare(Table* table, Col* condition_col, char* str_co
     for(current_row = table->first_row; current_row!=NULL; current_row=current_row->next_row){
         data_list_index = get_data_list_index(table, condition_col->name);
         should_add = false;
+        bool check_with_null = strcasecmp("NULL", str_condition) == 0;
 
         switch (col_type)
         {
         case INT:
-            if(current_row->int_list[data_list_index][0] == int_condition) should_add = true;
+            if ((check_with_null && !current_row->int_list[data_list_index]) ||
+            (!check_with_null && current_row->int_list[data_list_index] && current_row->int_list[data_list_index][0] == int_condition)) should_add = true; 
             break;
         case STRING:
-            if(strcmp(current_row->str_list[data_list_index], str_condition) == 0) should_add = true;
+            if ((check_with_null && !current_row->str_list[data_list_index]) ||
+            (!check_with_null && current_row->str_list[data_list_index] && strcmp(current_row->str_list[data_list_index][0],str_condition)==0)) should_add = true; 
             break;
         case DOUBLE:
-            if(compare_double(current_row->double_list[data_list_index][0], double_condition)  == 0) should_add = true;
+            if ((check_with_null && !current_row->double_list[data_list_index]) ||
+            (!check_with_null && current_row->double_list[data_list_index] && current_row->double_list[data_list_index][0] == double_condition)) should_add = true; 
             break;
         
         default: // ain't gonna happen 
@@ -80,15 +84,17 @@ FilteredRow* traverse_and_compare(Table* table, Col* condition_col, char* str_co
 FilteredRow* where_for_select(Table* table, Col* condition_col, char* str_condition, double double_condition, int int_condition, ColType col_type){
     // SELECT db needs to convert condition value before passing to this func
     // IMPORTANT, if type is not STRING then must pass NULL for str_condition
+    // IMPORTANT, condition col is NULL, always pass NULL to str_condition regardless of col_type
 
     FilteredRow* res = NULL;
     HashTable* ht_of_col = NULL;
     char* col_name = condition_col->name;
+    bool check_with_null = strcasecmp("NULL", str_condition) == 0;
 
     //check if condition col is indexed
     ht_of_col = get_ht_by_col_name(table->first_hash_table, col_name);
 
-    if(ht_of_col){
+    if(ht_of_col && !check_with_null){
         res = hash_lookup(ht_of_col, int_condition, str_condition);        
     }else{
         res = traverse_and_compare(table, condition_col, str_condition, double_condition, int_condition, col_type);
